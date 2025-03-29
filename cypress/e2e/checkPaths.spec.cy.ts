e80bd9ca1ed16292493ebeb650d0010f8d2cc6e8
Tales de Lima Filho <highhard2012@gmail.com>
2025-03-29 17:55:13 -0300

    implementation of class checkPath

--- cypress/e2e/checkPaths.spec.cy.ts
+++ cypress/e2e/checkPaths.spec.cy.ts
@@ -1,19 +1,16 @@
-import generalFunctions from "../support/pages/generalFunctions";
+import checkPaths from "../support/pages/checkPaths";
 
 describe("Checking Url paths", () => {
   before(() => {
     cy.visit("");
   });
   it("empty path clicking on logo", () => {
-    cy.get(".navbar-brand").click();
-    generalFunctions.checkUrl("https://uitestingplayground.com/");
+    checkPaths.emptyPathLogo();
   });
   it("Home path clicking on home button", () => {
-    cy.get(".nav-item").contains("Home").click();
-    generalFunctions.checkUrl("https://uitestingplayground.com/home");
+    checkPaths.homePath();
   });
   it("Resources path clicking in resources button", () => {
-    cy.get(".nav-item").contains("Resources").click();
-    generalFunctions.checkUrl("https://uitestingplayground.com/resources");
+    checkPaths.resourcesPath();
   });
 });
